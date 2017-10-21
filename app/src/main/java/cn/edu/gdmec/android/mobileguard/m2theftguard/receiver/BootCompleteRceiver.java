5fe8e70840960bbceba4e97ae7b007a28383a2fb
Sh96en9 <447951846@qq.com>
2017-10-21 20:51:11 +0800

    绑定SIM卡按钮已更新,安全号码保存完成.

---
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/receiver/BootCompleteRceiver.java
@@ -0,0 +1,18 @@
+package cn.edu.gdmec.android.mobileguard.m2theftguard.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import cn.edu.gdmec.android.mobileguard.App;
+
+/**
+ * Created by asus on 2017/10/20.
+ */
+
+public class BootCompleteRceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ((App)(context.getApplicationContext())).correctSIM();
+    }
+}
