cd32739dcd438aec75cbe7e86fac17a0d8c8cb37
Sh96en9 <447951846@qq.com>
2017-11-19 21:51:33 +0800

    手机杀毒(没有升级框)

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/SplashActivity.java
@@ -1,9 +1,11 @@
 package cn.edu.gdmec.android.mobileguard;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+import cn.edu.gdmec.android.mobileguard.m1home.HomeActivity;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.Myutils;
 import cn.edu.gdmec.android.mobileguard.m1home.utils.VersionUpdateUtils;
 
@@ -16,6 +18,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
         mVersion = Myutils.getVersion(getApplicationContext());
         mTvTextView = (TextView)findViewById(R.id.tv_splash_version);
         mTvTextView.setText("版本号"+mVersion);
@@ -27,5 +30,6 @@ public class SplashActivity extends AppCompatActivity {
                 versionUpdateUtils.getCloudVersion();
             }
         }.start();
+        startActivity(new Intent(this, HomeActivity.class));
     }
 }
